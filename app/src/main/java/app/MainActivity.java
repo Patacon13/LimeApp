a5b000645d1992473bf07fed59ab34ed3146c3c0
Tomas Assenza <tomyassenza@gmail.com>
2021-07-11 18:10:26 -0300

    removed debugging sysout

--- app/src/main/java/app/MainActivity.java
+++ app/src/main/java/app/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean httpGetToLibreMesh() throws InterruptedException, IOException {
         //FIXME: modificar google por la IP de LibreMesh
         String[] cmdLine = {"sh", "-c", "curl --head --silent --fail thisnode.info/app"};
-        System.out.println(cmdLine[0] + cmdLine[1] + cmdLine[2]);
         Process p1 = java.lang.Runtime.getRuntime().exec(cmdLine);
         int returnVal = p1.waitFor();
         return (returnVal == 0);
