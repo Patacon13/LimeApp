0c60f384491d9b9874fa3597c47326627e4eb374
Tomas Assenza <tomyassenza@gmail.com>
2021-07-15 03:27:50 -0300

    Generada simulacion de speechSynthesis con metodo vacio

--- app/src/main/java/app/LibreMesh.java
+++ app/src/main/java/app/LibreMesh.java
@@ -6,14 +6,17 @@ import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.limeapp.R;
 import com.example.limeapp.databinding.ActivityLibreMeshBinding;
+
 
 public class LibreMesh extends AppCompatActivity {
 
@@ -41,6 +44,7 @@ public class LibreMesh extends AppCompatActivity {
 
     private void configureNavigator(WebView webView) {
         WebSettings settings = webView.getSettings();
+        SpeechInterface speechInterface = new SpeechInterface();
         settings.setDomStorageEnabled(true);
         settings.setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient() {
@@ -52,6 +56,15 @@ public class LibreMesh extends AppCompatActivity {
             }
 
         });
+        webView.addJavascriptInterface(speechInterface, "speechSynthesis");
+    }
+
+    private class SpeechInterface {
+        //Voice reading
+        @JavascriptInterface
+        public void getVoices() {
+
+        }
     }
 
     private void runNavigator() {
@@ -66,7 +79,6 @@ public class LibreMesh extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             navegador.setWebContentsDebuggingEnabled(true);
         }
-
         navegador.loadUrl(NetworkAccessManager.getGateway(wifiManager));
     }
 
